050bd3daa273321ba39b2cb5a298e41381aef6fb
mrbiggles <mrbiggleshouseofgiggles@proton.me>
2023-02-10 21:52:15 +0000

    updated to deployed api url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ useEffect(() => {
     setLoading(true)
 
     try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-j57u.onrender.com/api/v1/post', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const startServer = async () => {
 
     try {
         connectDB(process.env.MONGODB_URL)
-        app.listen(8080, () => console.log('Server has started on port http://localhost:8080') )
+        app.listen(8080, () => console.log('Server has started on port https://dall-e-j57u.onrender.com/api/v1/post') )
     } catch (error) {
         console.log(error)
     }
